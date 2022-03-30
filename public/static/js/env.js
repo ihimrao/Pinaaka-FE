78f095c0ceeee912b5444d9a47a45f9236732210
imsarkar <60381004+imsarkar@users.noreply.github.com>
2022-03-29 23:33:45 -0700

    Update env.js

--- public/static/js/env.js
+++ public/static/js/env.js
@@ -1,5 +1,5 @@
 window.bricksConfig = {
     REACT_APP_FLAG_URL: '',
-    REACT_APP_BASE_URL: 'https://pinaakarewamped.herokuapp.com/api/',
+    REACT_APP_BASE_URL: 'https://pinaakaerver.herokuapp.com/api/',
     REACT_APP_BASE_IMAGE_URL: '',
 };
