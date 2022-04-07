df7b33d3f16fc7e3a599ec882a3f51e369bce6c8
imsarkar <hraj0105@gmail.com>
2022-04-07 11:08:37 -0700

    new

--- src/redux/sagas/requests/apiUrls.js
+++ src/redux/sagas/requests/apiUrls.js
@@ -1,4 +1,4 @@
-const serverUrl = window.bricksConfig ? window.bricksConfig.REACT_APP_BASE_URL : process.env.REACT_APP_BASE_URL;
+const serverUrl = window.bricksConfig ? process.env.REACT_APP_URI : process.env.REACT_APP_URI;
 const imgUrl = '';
 
 const apiUrls = {
